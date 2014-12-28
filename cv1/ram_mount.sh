fb8570ed005c0428072c51d9ef6fdba44c50f1b3
chcbaram <chcbaram@paran.com>
2014-12-28 17:55:51 +0000

    del

--- cv1/ram_mount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /mnt/ramdisk
-sudo mount -t tmpfs -o size=10M tmpfs /mnt/ramdisk/
-mkdir /mnt/ramdisk/img1
-mkdir /mnt/ramdisk/img2
-mkdir /mnt/ramdisk/web1
-mkdir /mnt/ramdisk/web2
-
--- cv1/stream_img1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mjpg_streamer -i "input_file.so -f /mnt/ramdisk/img1" -o "output_http.so -p 8080 -w /mnt/ramdisk/web1/"
